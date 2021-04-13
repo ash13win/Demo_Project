1dcf2aaf081f4a3962a002e3ee33e591044ec18e
shan-shaji <shan.shaji@yahoo.com>
2021-04-13 19:43:13 +0530

    added small styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { CSVDownload, CSVLink } from "react-csv";
 import React, { useState } from "react";
-
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 const headers = [
   { label: "Time", key: "time" },
   { label: "Date", key: "date" },
@@ -33,19 +34,40 @@ function App() {
 
   return (
     <div>
-      <button onClick={handleButtonClick}>Click me to generate time</button>
-      <p>{timeValue}</p>
-      <div>
-        <h3>Export data</h3>
-        <CSVLink
-          data={data}
-          headers={headers}
-          onClick={onDownloadLinkCick}
-          target="_blank"
-        >
-          Export to CSV
-        </CSVLink>
-      </div>
+      <Grid
+        container
+        justify="center"
+        className={{ flexGrow: 1 }}
+        spacing={2}
+        alignItems="center"
+      >
+        <Grid item xs={6}>
+          <Grid container justify="center">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleButtonClick}
+            >
+              Click Me
+            </Button>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={6} justify="center">
+          <p>{timeValue}</p>
+          <div>
+            <h3>Export data</h3>
+            <CSVLink
+              data={data}
+              headers={headers}
+              onClick={onDownloadLinkCick}
+              target="_blank"
+            >
+              Export to CSV
+            </CSVLink>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
